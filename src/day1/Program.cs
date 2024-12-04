6c4065d3ee4e0f9444395b9361eb828e5da8f738
Michael Farrelly <mikefarrelly@gmail.com>
2024-12-04 13:29:33 -0700

    Complete day 1

--- src/day1/Program.cs
+++ src/day1/Program.cs
@@ -1,52 +1,110 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
-Console.WriteLine("Hello, World!");
+// Day1();
 
-string input = """
-            3   4
-            4   3
-            2   5
-            1   3
-            3   9
-            3   3
-            """;
+Day2();
+
+void Day1() {
+// Console.WriteLine("Hello, World!");
+
+    string input = """
+                   3   4
+                   4   3
+                   2   5
+                   1   3
+                   3   9
+                   3   3
+                   """;
 
 // parse the input into lists
-string[] lines = input.Split(Environment.NewLine);
+    string[] lines = input.Split(Environment.NewLine);
 
+    List<List<int>> buckets = new List<List<int>>();
+    buckets.Add(new List<int>());
+    buckets.Add(new List<int>());
 
-List<List<int>> buckets = new List<List<int>>();
-buckets.Add(new List<int>());
-buckets.Add(new List<int>());
-
-foreach (var line in lines)
-{
-    var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-    for (var index = 0; index < parts.Length; index++)
+    foreach (var line in lines)
     {
-        var part = parts[index];
-        buckets[index].Add(int.Parse(part));
+        var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        for (var index = 0; index < parts.Length; index++)
+        {
+            var part = parts[index];
+            buckets[index].Add(int.Parse(part));
+        }
     }
-}
 
-buckets[0].Sort();
-buckets[1].Sort();
+    buckets[0].Sort();
+    buckets[1].Sort();
 
 // compare
-var deltas = new List<int>();
-foreach (var i in Enumerable.Range(0, buckets[0].Count)) {
-    deltas.Add(Math.Abs(buckets[0][i] - buckets[1][i]));
+    var deltas = new List<int>();
+    foreach (var i in Enumerable.Range(0, buckets[0].Count)) {
+        deltas.Add(Math.Abs(buckets[0][i] - buckets[1][i]));
+    }
+
+    Console.WriteLine(string.Join(",", lines));
+
+    foreach (var b in buckets)
+    {
+        Console.WriteLine(string.Join(",", b));
+    }
+
+    Console.WriteLine(string.Join(",", deltas));
+    Console.WriteLine(deltas.Sum());
 }
 
-Console.WriteLine(string.Join(",", lines));
+void Day2() {
+// Console.WriteLine("Hello, World!");
 
-foreach (var b in buckets)
-{
-    Console.WriteLine(string.Join(",", b));
+    string input = """
+                   3   4
+                   4   3
+                   2   5
+                   1   3
+                   3   9
+                   3   3
+                   """;
+
+// parse the input into lists
+    string[] lines = input.Split(Environment.NewLine);
+
+    List<List<int>> buckets = new List<List<int>>();
+    buckets.Add(new List<int>());
+    buckets.Add(new List<int>());
+
+    foreach (var line in lines)
+    {
+        var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        for (var index = 0; index < parts.Length; index++)
+        {
+            var part = parts[index];
+            buckets[index].Add(int.Parse(part));
+        }
+    }
+
+    // buckets[0].Sort();
+    // buckets[1].Sort();
+
+// compare
+    var similarity = new List<int>();
+    foreach (var i in Enumerable.Range(0, buckets[0].Count))
+    {
+        var value = buckets[0][i];
+        var cnt = buckets[1].Count(n => n == value);
+        similarity.Add(value * cnt);
+    }
+
+    Console.WriteLine(string.Join(",", lines));
+
+    foreach (var b in buckets)
+    {
+        Console.WriteLine(string.Join(",", b));
+    }
+
+    Console.WriteLine(string.Join(",", similarity));
+    Console.WriteLine(similarity.Sum());
+
 }
-
-Console.WriteLine(string.Join(",", deltas));
-Console.WriteLine(deltas.Sum());
