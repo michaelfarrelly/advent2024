1430b97e00a4c067a8c44e04856fa6b638020acf
Michael Farrelly <mikefarrelly@gmail.com>
2024-12-04 13:19:45 -0700

    day 1 part 1

--- src/day1/Program.cs
+++ src/day1/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 Console.WriteLine("Hello, World!");
+
+string input = """
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+            """;
+
+// parse the input into lists
+string[] lines = input.Split(Environment.NewLine);
+
+
+List<List<int>> buckets = new List<List<int>>();
+buckets.Add(new List<int>());
+buckets.Add(new List<int>());
+
+foreach (var line in lines)
+{
+    var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    for (var index = 0; index < parts.Length; index++)
+    {
+        var part = parts[index];
+        buckets[index].Add(int.Parse(part));
+    }
+}
+
+buckets[0].Sort();
+buckets[1].Sort();
+
+// compare
+var deltas = new List<int>();
+foreach (var i in Enumerable.Range(0, buckets[0].Count)) {
+    deltas.Add(Math.Abs(buckets[0][i] - buckets[1][i]));
+}
+
+Console.WriteLine(string.Join(",", lines));
+
+foreach (var b in buckets)
+{
+    Console.WriteLine(string.Join(",", b));
+}
+
+Console.WriteLine(string.Join(",", deltas));
+Console.WriteLine(deltas.Sum());
