73f8df87ae2f70c2449135bc0b6076b3e51850b6
Michael Farrelly <mikefarrelly@gmail.com>
2024-12-06 16:05:06 -0700

    Day 4, part 2 - Wip

--- src/day4/Program.cs
+++ src/day4/Program.cs
@@ -2,11 +2,12 @@
 //Console.WriteLine("Hello, World!");
 
 Day4Part1();
+Day4Part2();
 return;
 
 static void Day4Part1()
 {
-    Console.WriteLine("Day 3");
+    Console.WriteLine("Day 4");
     // load the data
     var lines = File
         .ReadLines("src/day4/inputFull.txt")
@@ -15,6 +16,19 @@ static void Day4Part1()
     Console.WriteLine(string.Join(",\n", lines));
     
     Console.WriteLine(FindString(lines, "XMAS"));
+
+}
+static void Day4Part2()
+{
+    Console.WriteLine("Day 3; Part 2");
+    // load the data
+    var lines = File
+        .ReadLines("src/day4/input.txt")
+        .ToList();
+    
+    Console.WriteLine(string.Join(",\n", lines));
+    
+    Console.WriteLine(FindXShape(lines, "MAS"));
 
 }
 
@@ -31,19 +45,7 @@ static int FindString(List<string> lines, string letters)
             // detect letters[0].
             if (lines[i][j] == letters[0])
             {
-                // start looking here out each direction.
-                // forwards
-                // if (lines[i][j..].StartsWith(letters))
-                // {
-                //     result++;
-                // }
-                //
-                // if (j - letters.Length >= 0 && lines[i]
-                //         .Substring(j - letters.Length)
-                //         .StartsWith(letters.Reverse().ToString()))
-                // {
-                //     result++;
-                // }
+                
                 var r = GetStringDirection(lines, j, i,  1, 0, letters.Length);
 
                 var l = GetStringDirection(lines, j, i,  -1, 0, letters.Length);
@@ -95,6 +97,90 @@ static int FindString(List<string> lines, string letters)
                 {
                     result++;
                 }
+            }
+        }   
+    }
+
+    return result;
+}
+
+
+static int FindXShape(List<string> lines, string letters)
+{
+    var result = 0;
+    var countX = lines[0].Length; // 10
+    var countY = lines.Count; // 10
+    
+    for (var i = 0; i < lines.Count; i++)
+    {
+        for (var j = 0; j < lines[i].Length; j++)
+        {
+            // detect letters[1].
+            if (lines[i][j] == letters[1])
+            {
+                // center "MAS".
+                
+                // var r = GetStringDirection(lines, j, i,  1, 0, letters.Length);
+
+                // var l = GetStringDirection(lines, j, i,  -1, 0, letters.Length);
+
+                
+                // diagonal /
+                var tr = GetStringDirection(lines, j - 1, i + 1,  1, -1, letters.Length);
+                
+                var tl = GetStringDirection(lines, j + 1, i + 1, -1, -1, letters.Length);
+                
+                var br = GetStringDirection(lines, j - 1, i - 1,  1,  1, letters.Length);
+                
+                var bl = GetStringDirection(lines, j + 1, i - 1,  -1, 1, letters.Length);
+                
+                // var b = GetStringDirection(lines, j, i,  0, 1, letters.Length);
+                // var t = GetStringDirection(lines, j, i,  0, -1, letters.Length);
+
+                
+                if (tr.StartsWith(letters) && br.StartsWith(letters))
+                {
+                    result++;
+                }
+                if (tl.StartsWith(letters) && bl.StartsWith(letters))
+                {
+                    result++;
+                }
+                
+                if (bl.StartsWith(letters) && bl.StartsWith(letters))
+                {
+                    result++;
+                }
+                
+                if (tl.StartsWith(letters) && bl.StartsWith(letters))
+                {
+                    result++;
+                }
+                // if (br.StartsWith(letters))
+                // {
+                //     result++;
+                // }
+                // if (bl.StartsWith(letters))
+                // {
+                //     result++;
+                // }
+                // if (b.StartsWith(letters))
+                // {
+                //     result++;
+                // }
+                // if (t.StartsWith(letters))
+                // {
+                //     result++;
+                // }
+                //
+                // if (r.StartsWith(letters))
+                // {
+                //     result++;
+                // }
+                // if (l.StartsWith(letters))
+                // {
+                //     result++;
+                // }
             }
         }   
     }
