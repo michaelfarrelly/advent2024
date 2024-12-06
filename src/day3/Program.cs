8078b2c162c802d4203a77ddb13d683e5d397e76
Michael Farrelly <mikefarrelly@gmail.com>
2024-12-06 12:54:28 -0700

    Day 3 Part 2

--- src/day3/Program.cs
+++ src/day3/Program.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 Console.WriteLine("Hello, World!");
 
 Day3Part1();
+Day3Part2();
 return;
 
 static void Day3Part1()
@@ -34,6 +35,53 @@ static void Day3Part1()
             
             var resultInner = values.Aggregate(1, (current, v) => current * v);
             result += resultInner;
+        }
+
+    }
+    Console.WriteLine(result);
+
+}
+
+
+static void Day3Part2()
+{
+    Console.WriteLine("Day 3;Part 2");
+    // load the data
+    var lines = File.ReadLines("src/day3/inputFull.txt");
+    
+    Console.WriteLine(string.Join(",", lines));
+    
+    // process the data
+    var result = 0;
+    var active = true;
+    foreach (var line in lines)
+    {
+        // get mul([0-9],[0-9]) matches
+        var reg = new Regex(@"(mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\))");
+        var matches = reg.Matches(line);
+        foreach (var m in matches)
+        {
+            if (active && m.ToString().StartsWith("mul"))
+            {
+                var values = m
+                    .ToString()
+                    .Replace("mul(", "")
+                    .TrimEnd(')')
+                    .Split(",")
+                    .Select(s => int.Parse(s))
+                    .ToList();
+
+                var resultInner = values.Aggregate(1, (current, v) => current * v);
+                result += resultInner;
+            }
+            else if (m.ToString().StartsWith("do("))
+            {
+                active = true;
+            }
+            else if (m.ToString().StartsWith("don't("))
+            {
+                active = false;
+            }
         }
 
     }
