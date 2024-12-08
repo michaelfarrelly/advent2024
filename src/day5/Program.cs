6efe97474e39d4ed83a296ad70f07e2fbf86cbbf
Michael Farrelly <mikefarrelly@gmail.com>
2024-12-07 21:32:54 -0700

    Day 5, part 2 - wip

--- src/day5/Program.cs
+++ src/day5/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Day 5");
 
 Day5Part1();
+Day5Part2();
 return;
 
 static void Day5Part1()
@@ -14,8 +15,8 @@ static void Day5Part1()
     // page ordering rules
     var (pageOrdering, updates) = Divide(lines);
 
-    Console.WriteLine(string.Join(",\n", pageOrdering));
-    Console.WriteLine(string.Join(",\n", updates));
+    // Console.WriteLine(string.Join(",\n", pageOrdering));
+    // Console.WriteLine(string.Join(",\n", updates));
 
     var result = 0;
     // updates
@@ -34,6 +35,62 @@ static void Day5Part1()
     Console.WriteLine(result);
 }
 
+static void Day5Part2()
+{
+    // load the data
+    var lines = File
+        .ReadLines("src/day5/inputFull.txt")
+        .ToList();
+    
+    
+    // page ordering rules
+    var (pageOrdering, updates) = Divide(lines);
+
+    // Console.WriteLine(string.Join(",\n", pageOrdering));
+    // Console.WriteLine(string.Join(",\n", updates));
+
+    var result = 0;
+    // updates
+    foreach (var u in updates)
+    {
+        var correct = IsUpdateCorrect(u, pageOrdering);
+        
+        if (!correct)
+        {
+            var updatedOrder = UpdateOrder(u, pageOrdering);
+            result += MiddleValue(updatedOrder);
+        }
+    }
+
+    Console.WriteLine(result);
+}
+
+static List<int> UpdateOrder(List<int> originalOrder, List<(int, int)> pageOrdering)
+{
+    List<int> updatedOrder = [..originalOrder.ToList()];
+    for (var i = 0; i < updatedOrder.Count; i++)
+    {
+        var n = updatedOrder[i];
+        var rules1 = pageOrdering.Where(p => p.Item1 == n).ToList();
+        var rules2 = pageOrdering.Where(p => p.Item2 == n).ToList();
+        // 4998 to high
+        // 4849 to low
+        
+        // check backwards rules?
+        foreach (var r in rules2)
+        {
+            var x = updatedOrder.IndexOf(r.Item1);
+            if (updatedOrder.Contains(r.Item1) && x > i)
+            {
+                // move r.Item1 to before current index.
+                updatedOrder.RemoveAt(x);
+                updatedOrder.Insert(i, r.Item1);
+            }
+        }
+    }
+    return updatedOrder;
+}
+
 static int MiddleValue(List<int> update)
 {
     var updateMiddleIndex = update.Count / 2;
@@ -50,7 +107,7 @@ static bool IsUpdateCorrect(List<int> update, List<(int, int)> pageOrdering)
         var rules1 = pageOrdering.Where(p => p.Item1 == n).ToList();
         var rules2 = pageOrdering.Where(p => p.Item2 == n).ToList();
         
-        Console.WriteLine("- {0} __  {1}", string.Join(",", rules1), string.Join(",", rules2));
+        // Console.WriteLine("- {0} __  {1}", string.Join(",", rules1), string.Join(",", rules2));
         
         // check forward rules
         foreach (var r in rules1)
